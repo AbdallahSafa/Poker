76db086f40aa1dce99abc889733e75c057bfc060
abdallahsafa <abdallah.safa@vanderbilt.edu>
2024-07-11 17:57:06 -0400

    Able to delete and retrieve games with id #.

--- src/main/java/org/example/pokes/pokes/PokerResource.java
+++ src/main/java/org/example/pokes/pokes/PokerResource.java
@@ -1,6 +1,8 @@
 package org.example.pokes.pokes;
 
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +22,20 @@ public class PokerResource {
     @GetMapping("/users/{username}/list-poker-games")
     public List<PokerGame> getPokerGames(@PathVariable String username) {
         return pokerGameRespository.findByUsername(username);
-
     }
+
+    @GetMapping("/users/{username}/list-poker-games/{id}")
+    public PokerGame getPokerGameById(@PathVariable String username , @PathVariable int id )
+    {
+        return pokerGameRespository.findById(id).orElseThrow();
+    }
+
+    @DeleteMapping("/users/{username}/list-poker-games/{id}")
+    public ResponseEntity<Void> deleteByID(@PathVariable String username , @PathVariable int id )
+    {
+        pokerGameRespository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
